0c0a5579b8d51e6d3d845bc38b1f8b018a403a32
MehdiChraief <73426208+MehdiChraief@users.noreply.github.com>
2020-10-25 13:53:50 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Chraief Mehdi 5TQI
+
+Exercices 2
+"""
+for index in range(5):
+    basic.show_string("la t° actuelle est de")
+    basic.show_string("" + str(input.temperature()))
---
+++ main.ts
@@ -0,0 +1,5 @@
+/** Chraief Mehdi 5TQI */
+for (let index = 0; index < 5; index++) {
+    basic.showString("la t° actuelle est de")
+    basic.showString("" + ("" + input.temperature()))
+}
